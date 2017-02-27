642eb18a560d337f5973512aa9c6e51344409e53
BaltimoreChad <cdombrowski85@gmail.com>
2017-02-26 23:40:22 -0500

    Initial Commit

--- lib/helpers.py
+++ lib/helpers.py
@@ -2,9 +2,15 @@
 File to hold various helper functions that will be used repeatedly.
 """
 import json
+import configparser
+import shodan
+import glob
+
+config = configparser.ConfigParser()
+config.read('pyonionscan.cfg')
 
 
-def jsonize_files(file: str):
+def jsonize_file(file: str):
     """
     Takes in a .json file from the output of onionrunner.py, converts it to json, and returns it.
     :param str file: the file to convert to json
@@ -13,3 +19,25 @@ def jsonize_files(file: str):
     with open(file, "rb") as fd:
         scan_result = json.load(fd)
         return scan_result
+
+
+def get_shodan_client():
+    """
+    Initializes a shodan client using the API defined in the pyonionscan.cfg file and returns the client.
+
+    :return shodan_client:
+    """
+    shodan_api_key = config['Shodan']['api_key']
+    shodan_client = shodan.Shodan(shodan_api_key)
+    return shodan_client
+
+
+def get_file_list():
+    """
+    Retrieves a file list from the onionscan_results directory defined in the pyonionscan.cfg file and returns
+    the list.
+    :return file_list:
+    """
+    onionscan_results = config['pyOnionScan']['onionscan_results']
+    file_list = glob.glob(f"{onionscan_results}/*.json")
+    return file_list
