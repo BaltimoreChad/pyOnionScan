121b18ee96453e2f22af38aa7786cce406b03270
BaltimoreChad <cdombrowski85@gmail.com>
2017-02-27 06:29:36 -0500

    Added hidden_services_graph.py

--- lib/helpers.py
+++ lib/helpers.py
@@ -5,7 +5,7 @@ import json
 import configparser
 import shodan
 import glob
-
+import os
 import sys
 
 config = configparser.ConfigParser()
@@ -16,6 +16,7 @@ def jsonize_file(file: str):
     """
     Takes in a .json file from the output of onionrunner.py, converts it to json, and returns it.
     :param str file: the file to convert to json
+
     :return scan_result:
     """
     with open(file, "rb") as fd:
@@ -25,7 +26,8 @@ def jsonize_file(file: str):
 
 def get_shodan_client():
     """
-    Initializes a shodan client using the API defined in the pyonionscan.cfg file and returns the client.
+    Initializes a shodan client using the API defined in the pyonionscan.cfg file and returns the client.  Exits if
+    api_key is not defined in config.
 
     :return shodan_client:
     """
@@ -40,7 +42,8 @@ def get_shodan_client():
 def get_file_list():
     """
     Retrieves a file list from the onionscan_results directory defined in the pyonionscan.cfg file and returns
-    the list.
+    the list.  Exits if onionscan_results is not defined in config.
+
     :return file_list:
     """
     onionscan_results = config['Paths']['onionscan_results']
@@ -49,3 +52,18 @@ def get_file_list():
         return file_list
     else:
         sys.exit("Onionscan results directory not found.  Please check your config.")
+
+
+def get_graph_directory():
+    """
+    Returns the graph directory where all graph files should be saved.  This value is stored in pyonionscan.cfg.
+    If the directory does not exist, it is created.  Exits if onionscan_graph is not defined in config.
+
+    :return graph_directory:
+    """
+    graph_directory = config['Paths']['onionscan_graph']
+    if graph_directory:
+        os.makedirs(graph_directory, exist_ok=True)
+        return graph_directory
+    else:
+        sys.exit("Onionscan graph directory not found.  Please check your config.")
